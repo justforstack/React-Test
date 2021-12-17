cc7afa4c72ca0d22aa6855554ed674bd5adc7b74
Rachna <justforstack031@gmail.com>
2021-12-18 01:19:29 +0530

    initial commit

---
+++ test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Test extends Component{
+    constructor(){
+    super();
+  
+    }
+    render(){
+        return(
+        <>
+<h1>This is a test file</h1>
+        </>
+        )
+    }
+}
